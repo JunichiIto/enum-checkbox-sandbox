7fb22efebebfa972a5f2c8f93ec47a5a57e1ae33
Junichi Ito <jit@sonicgarden.jp>
2015-08-27 22:53:13 +0900

    Refactoring.

--- app/models/car_selection.rb
+++ app/models/car_selection.rb
@@ -1,4 +1,4 @@
 class CarSelection < ActiveRecord::Base
   belongs_to :user
-  enum maker: %i(toyota nissan honda vw benz bmw audi)
+  enum maker: %i(toyota nissan honda)
 end
--- test/models/car_selection_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CarSelectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
